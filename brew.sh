812131e869118495a91e241dd7c2196c505f08a2
Pierre GOUDJO <1331326+pierregoudjo@users.noreply.github.com>
2023-08-17 10:00:13 +0200

    Set the shell to use for the install scripts

--- brew.sh
+++ brew.sh
@@ -1,5 +1,6 @@
+#!/usr/bin/env bash
 # Check for Homebrew and install if we don't have it
-if test ! $(which brew); then
+if ! type brew > /dev/null; then
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 # Update Homebrew recipes
