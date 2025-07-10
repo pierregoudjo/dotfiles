473d14a6ec016d501655bb73455363d74a96d71f
Pierre GOUDJO <1331326+pierregoudjo@users.noreply.github.com>
2025-07-10 15:30:01 +0200

    Make the script a bit more portable

--- install.sh
+++ install.sh
@@ -3,15 +3,21 @@
 echo "Setting up your workstation..."
 
 # Install MacOS applications
-if test "$(uname)" = "Darwin"; then
+# if test "$(uname)" = "Darwin"; then
   echo "--Install brew formulae"
   # Check for Homebrew and install if we don't have it
   if ! type brew > /dev/null; then
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
   fi
 
+  if test "$(uname)" = "Darwin"; then
+    BREW_EXECUTABLE="/opt/homebrew/bin/brew"
+  else
+    BREW_EXECUTABLE="/home/linuxbrew/.linuxbrew/bin/brew"
+  fi
+
   echo "---Create/update .zprofile file"
-  (echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') > ~/.zprofile
+  (echo; echo 'eval "$(' $BREW_EXECUTABLE ' shellenv)"') > ~/.zprofile
   echo "---Create/update .zprofile file DONE"
 
   echo "---Load .zprofile with brew path added"
@@ -29,7 +35,7 @@ if test "$(uname)" = "Darwin"; then
   brew bundle
   echo "-----Install and upgrade all dependencies from Brewfile DONE"
   echo "--Installation of brew formulae DONE"
-fi
+# fi
 
 # if test "$(uname)" = "Linux"; then
 #   echo "--Install apt-get packages on Linux"
@@ -50,25 +56,26 @@ echo "---Create .ssh directory if it doesn't exist"
 mkdir -pv ~/.ssh
 # Symlink dotfiles
 echo "--Symlink dotfiles"
-ln -shFv ${PWD}/.gitconfig ~/.gitconfig
-ln -shFv ${PWD}/.zshrc ~/.zshrc
-ln -shFv ${PWD}/.gitignore_global ~/.gitignore
-ln -shFv ${PWD}/.gitignore_global ~/.gitignore
-ln -shFv ${PWD}/starship/starship.toml ~/.config/starship.toml
-ln -shFv ${PWD}/.site-functions/ ~/.site-functions
-ln -shFv ${PWD}/gh/ ~/.config/gh
-ln -shFv ${PWD}/ghostty ~/.config/ghostty
-ln -shFv ${PWD}/zellij ~/.config/zellij
-ln -shFv ${PWD}/.terraformrc ~/.terraformrc
-ln -shFv ${PWD}/bin ~/bin
-ln -shFv ${PWD}/.ssh/config ~/.ssh/config
-ln -shFv ${PWD}/eza ~/.config/eza
-ln -shFv ${PWD}/bat/ ~/.config/bat
-ln -shFv ${PWD}/nvim/ ~/.config/nvim
-ln -shFv ${PWD}/aerc/ ~/.config/aerc
-ln -shFv ${PWD}/lazygit/ ~/.config/lazygit
-ln -shFv ${PWD}/vdirsyncer/ ~/.config/vdirsyncer
-ln -shFv ${PWD}/khard/ ~/.config/khard
+ln -svf ${PWD}/.gitconfig ~/.gitconfig
+ln -svf ${PWD}/.zshrc ~/.zshrc
+ln -svf ${PWD}/.gitignore_global ~/
+ln -svf ${PWD}/.gitignore_global ~/
+ln -svf ${PWD}/starship/starship.toml ~/.config/
+ln -svf ${PWD}/.site-functions ~/
+ln -svf ${PWD}/gh/ ~/.config/
+ln -svf ${PWD}/ghostty ~/.config/
+ln -svf ${PWD}/zellij ~/.config/
+ln -svf ${PWD}/.terraformrc ~/
+ln -svf ${PWD}/bin ~/
+ln -svf ${PWD}/.ssh/config ~/.ssh/
+ln -svf ${PWD}/eza ~/.config/
+ln -svf ${PWD}/bat/ ~/.config/
+ln -svf ${PWD}/nvim/ ~/.config/
+ln -svf ${PWD}/aerc/ ~/.config/
+ln -svf ${PWD}/lazygit/ ~/.config/
+ln -svf ${PWD}/vdirsyncer/ ~/.config/
+ln -svf ${PWD}/khard/ ~/.config/
+ln -svf ${PWD}/khal/ ~/.config/
 
 if test "$(uname)" = "Darwin"; then
   mkdir -pv ~/Library/Application\ Support/Sublime\ Text/Packages/
