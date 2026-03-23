27d25c705b15d7b56a45c8c20ffbc05dd31c4b38
Pierre GOUDJO <1331326+pierregoudjo@users.noreply.github.com>
2026-03-23 14:10:53 +0100

    Set up mini.clue to preview next actions

--- nvim/init.lua
+++ nvim/init.lua
@@ -22,6 +22,48 @@ require('mini.notify').setup()
 require('mini.basics').setup()
 require('mini.diff').setup()
 require('mini.git').setup()
+local miniclue = require('mini.clue')
+miniclue.setup({
+  triggers = {
+    -- Leader triggers
+    { mode = { 'n', 'x' }, keys = '<Leader>' },
+
+    -- `[` and `]` keys
+    { mode = 'n', keys = '[' },
+    { mode = 'n', keys = ']' },
+
+    -- Built-in completion
+    { mode = 'i', keys = '<C-x>' },
+
+    -- `g` key
+    { mode = { 'n', 'x' }, keys = 'g' },
+
+    -- Marks
+    { mode = { 'n', 'x' }, keys = "'" },
+    { mode = { 'n', 'x' }, keys = '`' },
+
+    -- Registers
+    { mode = { 'n', 'x' }, keys = '"' },
+    { mode = { 'i', 'c' }, keys = '<C-r>' },
+
+    -- Window commands
+    { mode = 'n', keys = '<C-w>' },
+
+    -- `z` key
+    { mode = { 'n', 'x' }, keys = 'z' },
+  },
+
+  clues = {
+    -- Enhance this by adding descriptions for <Leader> mapping groups
+    miniclue.gen_clues.square_brackets(),
+    miniclue.gen_clues.builtin_completion(),
+    miniclue.gen_clues.g(),
+    miniclue.gen_clues.marks(),
+    miniclue.gen_clues.registers(),
+    miniclue.gen_clues.windows(),
+    miniclue.gen_clues.z(),
+  },
+})
 require('oil').setup()
 
 -- Global options
@@ -30,8 +72,6 @@ vim.cmd.colorscheme 'alabaster'
 vim.api.nvim_set_hl(0, 'MiniPickMatchCurrent', {bg = '#98fb98'})
 vim.o.number         = true
 vim.o.relativenumber = true
-vim.o.updatetime     = 250
-vim.o.timeoutlen     = 300
 vim.o.list           = true
 vim.opt.listchars    = { tab = '» ', trail = '·', nbsp='␣' }
 vim.o.inccommand     = 'split'
